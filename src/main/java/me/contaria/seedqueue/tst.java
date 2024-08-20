a1e580b683944dc40d2aba68abdf0bdf3299b8eb
cylorun <allidabbi@gmail.com>
2024-08-20 23:26:27 +0000

    removed temp test file

--- src/main/java/me/contaria/seedqueue/tst.java
+++
@@ -1,9 +0,0 @@
-package me.contaria.seedqueue;
-
-public class tst {
-    public static void main(String[] args) {
-        double c = SeedQueueConfig.CPUClockSpeed.getCPUClockSpeed();
-        System.out.println(c);
-    }
-
-}
